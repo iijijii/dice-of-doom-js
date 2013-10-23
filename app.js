f65a08d8481b72ce9e1d27ef455e71f1acc1017d
iijijii <lilysatoo@gmail.com>
2013-10-23 09:28:40 +0900

    jqueryを追加

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 //N
-var partition = 2
-var maxDices = 3
-var board = new Array();
+var partition = 2;
+var maxDices = 3;
 var turn = "A";
 
-//盤面、サイコロを配置
-function start(){
+$(function(){
+	start()
+}); 
+function start(){	
+	//盤面、サイコロを配置
+	var board = new Array();
 	makeMasu();
 	draw();
+
 }
 
 function makeMasu(){
@@ -17,35 +21,36 @@ function makeMasu(){
             board[x][y]= desideWhosePlace()+desideNumOfDices();
         }
     }
-}
+
+    //各ヘックスのサイコロの数を決める
+	function desideNumOfDices(){
+		var numberOfDices = Math.floor(Math.random() * (maxDices))+ 1;
+		return numberOfDices;
+	}
+	//各ヘックスがだれの陣地かを決める
+	function desideWhosePlace(){
+		var randomNumber = Math.floor(Math.random() * 2);
+		if(randomNumber == 0){return "A";}
+		else{return "B";}
+	}
+
+};
 
 function draw(){ 
 	var line='';
     for(var y=0;y<partition;y++){
-            for(var x=0;x<partition;x++){line+=board[x][y];}
-                    line+="<br>"
+        for(var x=0;x<partition;x++){line+=board[x][y];}
+        line+="<br>"
     }        
     document.getElementById("board").innerHTML=line;
     document.getElementById("turn").innerHTML="turn : "+turn;
 }
 
-//各ヘックスのサイコロの数を決める
-function desideNumOfDices(){
-	var numberOfDices = Math.floor(Math.random() * (maxDices + 1));
-	return numberOfDices;
-}
-
-//各ヘックスがだれの陣地かを決める
-function desideWhosePlace(){
-	var randomNumber = Math.floor(Math.random() * 2);
-	if(randomNumber == 0){return "A";}
-	else{return "B";}
-}
-
-
+//////////////////////////////////////////////////////////////
 function attack(){
+
 	if(canAttack){
-		changePosition();
+		changePosition(attackX,attackY,aimX,aimY);
 	}
 	else{
 		supply();
@@ -62,6 +67,7 @@ function canAttack(){
 function changePosition(){
 }
 
+/////////////////////////////////////////////////////////////////
 //補給する。
 function supply(){}
 
@@ -69,4 +75,3 @@ function changeTurn(){
 	if(turn="A"){turn ="B"}
 	else{turn = "A"}
 }
-
