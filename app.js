4c4ee9d692a2bf5721f03fb8ca661c2fcead42e8
iijijii <lilysatoo@gmail.com>
2013-10-23 09:45:44 +0900

    二次元配列を一次元に変更

--- app.js
+++ app.js
@@ -15,32 +15,23 @@ $(function(){
 	draw();
 
 	function makeMasu(){
-		for(var x=0;x<partition;x++){
-	        board[x]=new Array();
-	        for(var y=0;y<partition;y++){
-	            board[x][y]= desideWhosePlace()+desideNumOfDices();
-	        }
-	    }
-	}
-
-    //各ヘックスのサイコロの数を決める
-	function desideNumOfDices(){
+	    for(var i=0;i<partition*partition;i++){
 		var numberOfDices = Math.floor(Math.random() * (maxDices))+ 1;
-		return numberOfDices;
-	}
-	//各ヘックスがだれの陣地かを決める
-	function desideWhosePlace(){
-		var randomNumber = Math.floor(Math.random() * 2);
-		if(randomNumber == 0){return "A";}
-		else{return "B";}
+		var player= Math.floor(Math.random() * 2);
+		
+    	var masu = new masuData(player,numberOfDices,i);
+    	board.push(masu);
+    	}	    
 	}
 
 	function draw(){ 
 		var line='';
-	    for(var y=0;y<partition;y++){
-	        for(var x=0;x<partition;x++){line+=board[x][y];}
-	        line+="<br>"
-	    }        
+	    for(var j=0;j<partition*partition;j++){
+        	line += turn[board[j].turn] + board[j].dices.toString();
+        	if(j%2==1){
+        		line+="<br>" ;
+        	}
+    	}   
 	    document.getElementById("board").innerHTML=line;
 	    document.getElementById("turn").innerHTML="turn : "+turn;
 	}
